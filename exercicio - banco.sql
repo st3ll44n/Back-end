00d6cef4a8a8617100e5f472a81f51d5fc751313
Stella Andrade <stellanaziozeno@gmail.com>
2025-02-10 17:45:00 -0300

    exercicio - banco

---
+++ exercicio - banco.sql
@@ -0,0 +1,12 @@
+create database banco;
+use banco;
+create table contas (
+id INT(6) auto_increment NOT NULL PRIMARY KEY,
+nome VARCHAR(20) NOT NULL,
+sobrenome VARCHAR(20) NOT NULL,
+saldo INT(6.2),
+dataNas DATE
+);
+insert into contas (id,nome,sobrenome,saldo,dataNas) values (015544,"Stella","Andrade",5241.52,08-09-2004);
+insert into contas (id,nome,sobrenome,saldo,dataNas) values (015174,"Gustavo","Nascimento",5601.52,01-14-2005);
+insert into contas (id,nome,sobrenome,saldo,dataNas) values (015614,"William","Ferreira",5871.52,01-10-2003);
