cae6228fd5dd68efde5daa1c4b0b596cda2b924c
Stella Andrade <stellanaziozeno@gmail.com>
2025-02-27 17:39:34 -0300

    Exercicio - Introdução PHP

---
+++ PHP/introducao_PHP/index_exercicio.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/style.css">
+    <meta name="viewport" content="width=[device-width], initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+   
+    <header>
+        <h2>Livros.com</h2>
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="">Catálogo</a>
+                <a href="">Sobre Nós</a>
+            </nav>
+    </header>
+    
+    <main>
+        <h1>Leitura para todos as idades e gostos!</h1><br> 
+        
+        <section class="book">
+        <div class="book-image">
+            <img src="images/a-menina_que_roubava_livros.jpeg" alt="Capa do Livro">
+        </div>
+        <div class="book-details">
+            <h2 class="book-title">A menina que roubava livros</h2><br>
+            <p class="book-author">Autor: Markus Zusak</p><br>
+            <p class="book-description">
+        A Menina que Roubava Livros é um romance escrito por Markus Zusak. A história é narrada pela Morte e se passa na Alemanha durante a Segunda Guerra Mundial. O livro acompanha a vida de Liesel Meminger, uma menina de 9 anos que é acolhida por uma família adotiva depois de perder a mãe e o irmão. Ao longo da trama, Liesel encontra consolo nos livros, que começa a roubar sempre que surge uma oportunidade.
+            </p><br>
+            <button class="buy-button">Comprar</button>
+        </div>
+    </section>
+
+    </main>
+
+    <footer>
+        <h4>Todos os direitos autorais estão reservados a empresa @Livros.com</h4>
+    </footer>
+    
+</body>
+</html>
